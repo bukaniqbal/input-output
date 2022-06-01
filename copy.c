e879ddc119652ed568145ed26f7366c1dd0ff183
bukaniqbal <90101765+bukaniqbal@users.noreply.github.com>
2022-06-01 20:53:42 +0700

    Create copy.c

---
+++ copy.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include <fcntl.h> 
+int main(int argc,char* argv[])
+{ 
+int fd, ed, sz, tz; 
+char *c = (char *) calloc(100, sizeof(char)); 
+  
+fd = open(argv[1], O_RDONLY); 
+if (fd < 0) { perror("r1"); exit(1); } 
+  
+sz = read(fd, c, 10); 
+c[sz] = '\0'; 
+ 
+  
+ed = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644); 
+if (ed < 0) 
+{ 
+    perror("r1"); 
+    exit(1); 
+} 
+  
+tz = write(ed, c, strlen(c)); 
+  
+close(ed); 
+
+}
